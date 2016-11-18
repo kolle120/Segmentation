9ac04d5177ffcccf07a486ee16c4fdbab8f96432
Emily Kollenbroich <Emily Kollenbroich>
2016-11-18 16:56:50 -0600

    wow I suck at spelling

--- heart_seg.py
+++ heart_seg.py
@@ -1,12 +1,12 @@
 from keras.models import Sequential
 from keras.layers import Dropout, Activation
-from keras.layers import Convolutionl2D, MaxPooling2D
+from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 
 model = Sequential()
-model.add(Convolutioanl2D(32, 3, 3, border_mode='valid', input_shape=(1, 224, 224)))
+model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(1, 224, 224)))
 model.add(Activation('relu'))
-model.add(Convolutioanl2D(32, 3, 3))
+model.add(Convolution2D(32, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
@@ -18,11 +18,11 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Convolutioanl2D(512, 1, 1, init='uniform', activation='relu', border_mode='valid'))
+model.add(Convolution2D(512, 1, 1, init='uniform', activation='relu', border_mode='valid'))
 
-model.add(Convolutioanl2D(512, 1, 1, init='uniform', activation='relu', border_mode='valid'))
+model.add(Convolution2D(512, 1, 1, init='uniform', activation='relu', border_mode='valid'))
 
-model.add(Convolutioanl2D(1, 1, 1, init='uniform', activation='relu', border_mode='valid'))
+model.add(Convolution2D(1, 1, 1, init='uniform', activation='relu', border_mode='valid'))
 #output image = 56x56
 
 #upsample the output to match input dimensions = 224x224
